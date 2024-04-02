58bd82957754bb9a53952a1557c6d33f9aa0d682
milkwitheyes <154975761+milkwitheyes@users.noreply.github.com>
2024-04-02 16:20:09 +0600

    Add files via upload

---
+++ hw5.month3/main.js
@@ -0,0 +1,31 @@
+function calculate() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var operator = document.getElementById("operator").value;
+    var result;
+
+    switch(operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            result = num1 / num2;
+            break;
+        case "^":
+            result = Math.pow(num1, num2);
+            break;
+        case "sqrt":
+            result = Math.pow(num1, 1/num2); // Формула для вычисления корня
+            break;
+        default:
+            result = "неподдерживаемая операция";
+    }
+
+    document.getElementById("result").innerText = "Результат: " + result;
+}
